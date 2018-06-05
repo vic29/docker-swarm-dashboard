e1420ba91f0b7b29aa80aeaaa17e412c7d9c09c2
Barnabás Szabó <barnabas.szabo@loxon.eu>
2018-06-05 08:41:09 +0200

    vLmi

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -9,6 +9,5 @@ yarn install
 npm run build
 
 cd ..
-time docker build --pull -t szabobar/swarm-dashboard:1.1 -t szabobar/swarm-dashboard:latest .
-docker push szabobar/swarm-dashboard:1.1
-docker push szabobar/swarm-dashboard:latest
+time docker build --pull -t szabobar/swarm-dashboard:1.1.debug  .
+docker push szabobar/swarm-dashboard:1.1.debug
