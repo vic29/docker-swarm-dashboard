d8609939a2ae5f4500a4eca666ff3623d42a39aa
vic29 <szentesi.vik@gmail.com>
2018-06-06 10:37:14 +0200

    Querying logs with given max rows only

--- docker/docker_log.js
+++ docker/docker_log.js
@@ -1,6 +1,6 @@
 const env = require('../environment/envHandler.js');
 const Docker = require('dockerode');
-
+const dockerLogsMaxRowNum = env.get('DOCKER_LOGS_MAX_ENTRY_NUM') || 2000
 // https://gist.github.com/jupeter/b39e11521452129af2af85cc855c91d7
 // majd service docker restart
 
@@ -20,7 +20,8 @@ module.exports = function (id, streamFunction) {
   item.logs({
     follow: true,
     stdout: true,
-    stderr: true
+    stderr: true,
+    tail: dockerLogsMaxRowNum
   }, function (err, stream) {
     if (err) {
       return console.log(err.message);
